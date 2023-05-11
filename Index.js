46633aeea546597b73179a1010d43e71a030cc6f
MateoGuevara09 <97854957+MateoGuevara09@users.noreply.github.com>
2023-05-11 11:11:28 -0300

    falta mostrar detalles y mostrar peliculas

--- Index.js
+++ Index.js
@@ -1,58 +1,27 @@
 import PersonajesService from "./Services/personajes-services.js"
 import peliculasyseriesservices from "./Services/peliculasyseries-services.js"
-//import { Express } from "express";
-
-async function getAll(){
-    let svc = new PersonajesService();
-    let data = await svc.getAll();
-    let svc2 = new peliculasyseriesservices();
-    let data2 = await svc2.getAll();
-    console.log(data)
-    console.log(data2)
-}
-async function getbyid(){
-    let svc = new PersonajesService();
-    let data = await svc.getById(1);
-    let svc2 = new peliculasyseriesservices();
-    let data2 =  await svc2.getById(1);
-    console.log(data)
-    console.log(data2)
-}
-async function deleteByIdPersonaje(){
-    let svc = new PersonajesService();
-    let data = await svc.deleteById(3)
-    console.log(data);
-}
-async function UPDATEPersonaje(){
-    let svc = new PersonajesService();
-    let data;
-    let Personaje;
-    Personaje = await svc.getById(3)
-    if(Personaje != null){
-        data = await svc.update(Personaje)
-        console.log(data)
-    }else{
-        console.log('peronsaje');
-        console.log(Personaje);
-    }
-}
-async function deleteByIdPelicula(){
-    let svc = new peliculasyseriesservices();
-    let data = await svc.deleteById(7)
-    console.log(data);
-}
-async function updatePelicula(){
-    let svc = new peliculasyseriesservices();
-    let data;
-    let peliculasyseries;
-    peliculasyseries = await svc.getById(5)
-
-    if(peliculasyseries != null){
-        data = await svc.update(peliculasyseries)
-        console.log(data)
-    }else{
-        console.log('pelilculo');
-        console.log(peliculasyseries);
-    } 
-}
-await updatePelicula();
+import  Express  from "express";
+const app = Express();
+const port = 5000;
+const svc1 = new PersonajesService();
+const svc2 = new peliculasyseriesservices()
+//principal
+app.get('/',async (req,res) =>{
+    res.send("pone /characters para mostrar personajes o /movies para mostrar las peliculas")
+})
+//personajes
+app.get('/characters',async (req,res) =>{
+    const peliculasyseries = await svc1.getNombreImagenId();
+    return res.status(200).json(peliculasyseries);
+})
+//app.get('/characters/:idpelicula'){
+ //   const personaje = await svc1.getPersonajePorCaracteristica();
+//}
+app.get('/characters/:id',async (req,res) =>{
+    const personaje = await svc1.getById(req.params.id);
+    return res.status(200).json(personaje)
+});
+//server
+app.listen(port,() =>{
+    console.log('ESCUCHANDO PORT 5000')
+})
--- Services/personajes-services.js
+++ Services/personajes-services.js
@@ -1,58 +1,83 @@
 import config from "../dbconfig.js";
 import sql from 'mssql'
 
-class PersonajesService{
+class PersonajesService {
     //getall
 
     getAll = async () => {
 
         let returnArray = null;
-    
-        console.log('Estoy en: PizzaService.getAll()');
-    
+
+        console.log('Estoy en: PERSONAJESERVIES.getAll()');
+
         try {
-    
-            let pool   = await sql.connect(config);
-    
+
+            let pool = await sql.connect(config);
+
             let result = await pool.request().query("SELECT * from Personajes");
-    
+
             returnArray = result.recordsets[0];
-    
+
         }
-    
+
         catch (error) {
-    
+
             console.log(error);
-    
+
         }
-    
+
         return returnArray;
-    
+
     }
     //getbyid
     getById = async (id) => {
         let returnEntity = null;
         console.log('Personajeservice.getbyid');
         try {
-            let pool   = await sql.connect(config);
+            let pool = await sql.connect(config);
             let result = await pool.request()
-                                .input('pId', sql.Int, id)
-                                .query('SELECT * FROM Personajes WHERE id = @pId');
+                .input('pId', sql.Int, id)
+                .query('SELECT * FROM Personajes WHERE id = @pId');
             returnEntity = result.recordsets[0][0];
         } catch (error) {
             console.log(error);
         }
         return returnEntity;
     }
+    //getnombreimagenid
+    getNombreImagenId = async () => {
+        let returnArray = null;
+
+        console.log('Estoy en: PERSONAJESERVIES.nombreimagenid()');
+
+        try {
+
+            let pool = await sql.connect(config);
+
+            let result = await pool.request().query("SELECT ID,Imagen,Nombre from Personajes");
+
+            returnArray = result.recordsets[0];
+
+        }
+
+        catch (error) {
+
+            console.log(error);
+
+        }
+
+        return returnArray;
+    }
+    
     //deletebyid
     deleteById = async (id) => {
         let rowsAffected = 0;
         console.log('Personajeservice.deleteById');
         try {
-            let pool   = await sql.connect(config);
+            let pool = await sql.connect(config);
             let result = await pool.request()
-                                .input('pId', sql.Int, id)
-                                .query('DELETE FROM Personajes WHERE id = @pId');
+                .input('pId', sql.Int, id)
+                .query('DELETE FROM Personajes WHERE id = @pId');
             rowsAffected = result.rowsAffected;
         } catch (error) {
             console.log(error);
@@ -67,12 +92,12 @@ class PersonajesService{
             var edad = 2
             let pool = await sql.connect(config);
             let result = await pool.request()
-                .input('pImagen', sql.NVarChar(500) , Personaje?.Imagen ?? '')
-                .input('pNombre', sql.NVarChar(50)  , Personaje?.Nombre ?? '')
-                .input('pEdad'    , sql.Int , edad ?? 0)
-                .input('pPeso', sql.Float , Personaje?.peso ?? 0)
+                .input('pImagen', sql.NVarChar(500), Personaje?.Imagen ?? '')
+                .input('pNombre', sql.NVarChar(50), Personaje?.Nombre ?? '')
+                .input('pEdad', sql.Int, edad ?? 0)
+                .input('pPeso', sql.Float, Personaje?.peso ?? 0)
                 .input('pHistoria', sql.NVarChar(500), Personaje?.historia ?? '')
-                .input('pId'         , sql.Int   , Personaje?.ID ?? 0)
+                .input('pId', sql.Int, Personaje?.ID ?? 0)
                 .query(`UPDATE Personajes SET Imagen = @pImagen, Nombre = @pNombre, Edad = @pEdad, Peso = @pPeso, Historia = @pHistoria WHERE Id = @pId`);
             rowsAffected = result.rowsAffected;
         } catch (error) {
@@ -81,4 +106,6 @@ class PersonajesService{
         return rowsAffected;
     }
 }
+//getdetallespersonajes
+
 export default PersonajesService
